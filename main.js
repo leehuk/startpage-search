cc4adceea1223d02ea6a27672995cbb00718d974
Lee H <lee@leeh.uk>
2017-12-29 19:33:42 +0000

    - add hotkeys via 1-9 keys and build default menu

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // configuration settings
 const menuiconurl = 'icons/';
-var maxmenucount = 5;
+var maxmenucount = 5; // maximum 9
 
 // internal variables
 var menusearch = '';
 var menutimeout;
+var menuhotkey = [];
 
 // Add hotkeys, letters for menus, numbers for items within a menu
 $(document).ready(function() {
+	menuBuild();
+
 	$('body').on("keydown", function(ev) {
 		window.clearTimeout(menutimeout);
 
@@ -19,9 +22,12 @@ $(document).ready(function() {
 			menutimeout = window.setTimeout(menuBuild, 350);
 			ev.preventDefault();
 		// a-z || 0-9
-		} else if((ev.keyCode >= 65 && ev.keyCode <= 90) || (ev.keyCode >= 48 && ev.keyCode <= 57)) {
+		} else if(ev.keyCode >= 65 && ev.keyCode <= 90) {
 			menusearch += ev.key;
 			menutimeout = window.setTimeout(menuBuild, 350);
+		} else if (ev.keyCode >= 49 && ev.keyCode <= 49+maxmenucount-1) {
+			console.log("Redirecting to " + menuhotkey[ev.keyCode-49]);
+			document.location = menuhotkey[ev.keyCode-49];
 		} else {
 			console.log("Ignoring " + ev.key + " " + ev.keyCode);
 		}
@@ -31,6 +37,7 @@ $(document).ready(function() {
 function menuClear() {
 	var content = $('div.content');
 	$(content).empty();
+	menuhotkey = [];
 }
 
 function menuBuild() {
@@ -41,11 +48,14 @@ function menuBuild() {
 
 	menuregex = new RegExp(menusearch, 'i');
 
-	menu.forEach(function(element) {
+	menu.some(function(element) {
 		if(element.name.search(menuregex) >= 0) {
 			menuBuildItem(element);
-			console.log('matched: ' + element.name);
+			menuhotkey[menucount] = element.url;
+			menucount++;
 		}
+
+		return menucount >= maxmenucount;
 	});
 
 	console.log(menusearch);
